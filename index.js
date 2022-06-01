84062a8da44ca774d3b5da0820b1b1fc50909c0b
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2022-06-01 16:12:52 +0600

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//verify jwt
+//verify json web token
 const verifyToken = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
